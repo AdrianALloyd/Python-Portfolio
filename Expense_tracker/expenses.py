933ea380efc1bd3080e2dd9ad24b3ddcf25b6b28
AdrianALloyd <AdrianALloyd@outlook.com>
2024-11-07 15:40:02 +0000

    finished program, made main loop, added file handling

--- Expense_tracker/expenses.py
+++ Expense_tracker/expenses.py
@@ -1,6 +1,6 @@
 
-# output welcome message
-print("Welcome to Expense Tracker")
+import json
+
 
 expenses = {
     "152" : {"Bills" : "Paid gas and electric"},
@@ -11,15 +11,18 @@ expenses = {
 
 def create_expense():
     # prompt for an expense ammount
-    expense = format(float(input("Please enter an Expense to track:\n")), '.2f')
+    expense = format(float(input("Please enter an Expense (e.g 20.50)to track:\n")), '.2f')
+
+    if float(expense) < 0:
+        print("I'm sorry but that is not a valid number, defaulting value to 0")
+        expense = 0
     #prompt for expense category
     category = input("Please enter what category this expense falls into:\n")
     #prompt for expense description
     description = input("Please give a brief description for this expense:\n")
 
     expenses[expense] = {category : description}
-
-    print(expenses)
+    print("Expense successfully added!\n")
 #store data
 
 def total():
@@ -55,7 +58,7 @@ def details():
         for y in expenses[x]:
             print(f"Cost: £{x}, For: {y}, Notes: {expenses[x][y]}")
 
-def summary():
+def summary(): #formatting, and calling functions to print the summary
     print("#####--Start-#####")
     print("#####--Categories--#####")
     categories()
@@ -64,10 +67,92 @@ def summary():
     print("#####--Total--#####")
     total()
 
-summary()
+def save():
+    #savejson = json.dumps(expenses) 
+
+    with open("my_expenses.json", "w") as fp:
+        json.dump(expenses, fp)
+
+    print("File successfully saved")
+
+def load():
+    f = open("my_expenses.json")
+    load_file = json.load(f)
+    print(f"file:\n{load_file}")
+    clear()
+    expenses.update(load_file)
+    print("File successfully loaded")
+    
+
+def delete():
+    summary()
+    deleting = input("Which item do you want to delete from your list, please type the ammount:\n")
+    if deleting == "":
+        expenses.clear()
+    elif deleting in expenses:
+        del expenses[deleting]
+        print("Entry successfully deleted\n")
+    else:
+        print(f"Sorry, could not find a match for {deleting} in your list")
+
+def clear():
+    expenses.clear()
+    
+    print("Your list has been cleared!")
+
+#program start user welcome
+print("Welcome to Expense Tracker")
 
 
-print("Thank you for using Expense Track, Have a nice day!")
+while True:
+    command = input("How can we help you today? type 'help' for a list of options\n")
+    match command:
+
+        case 'help':
+            print("You can type: \nhelp - To show the help menu.")
+            print("new - To add a new expense.")
+            print("total - To show the total of expenses.")
+            print("details - To show all the details of your expenses")
+            print("category - To view a breakdown of expenses by category.")
+            print("summary - To see a summary of all expenses.")
+            print("save - To save your expense list")
+            print("load - To load your expense list")
+            print("delete - To delete an item from your expense list")
+            print("clear - To clear your expense list")
+            print("exit - To exit the expense program")
+            
+        case 'new':
+            create_expense()
+            
+        case 'total':
+            total()
+            
+        case 'details':
+            details()
+            
+        case 'category':
+            categories()
+            
+        case 'summary':
+            summary()
+            
+        case 'save':
+            save()
+        case 'load':
+            load()
+        case 'delete':
+            delete()
+        case 'clear':
+            clear()
+            print("Your expenses list has been cleared!\n")
+        case 'exit':
+            break
+        case _:
+            print("Sorry, that is not a recognised command, type 'help' to see a list of available commands\n")
+            
+
+
+print("Thank you for using Expense Track, Have a nice day!")    
 # create_expense()
 # create_expense()
 
